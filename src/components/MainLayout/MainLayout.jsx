6693ea5a20c5d891eb25fff3072b07c4558e98e1
Sergei <kvakazuabr@yandex.ru>
2025-06-02 22:50:46 +0300

    initial commit

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import cls from "./MainLayout.module.css";
+
+export const MainLayout = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className={cls.mainLayout}>
+      <header>Header</header>
+      <div className={cls.mainWrapper}>
+        <main className={cls.main}>main</main>
+        <footer className={cls.footer}>
+          React Question Cards Application | {currentYear}
+          <br />
+          by Sergei Ko
+        </footer>
+      </div>
+    </div>
+  );
+};
