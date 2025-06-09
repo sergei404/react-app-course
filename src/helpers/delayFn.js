8ba959318dc3380367fca7927a42fb0f938832f2
Sergei <kvakazuabr@yandex.ru>
2025-06-09 21:50:52 +0300

    delayFn, useFetch hook

---
+++ src/helpers/delayFn.js
@@ -0,0 +1,1 @@
+export const delayFn = async (delay = 1000) => await new Promise((res) => setTimeout(res, delay));
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,34 +1,46 @@
 import { useEffect, useState } from "react";
-// import { QuestionCard } from "../../components/QuestionCard";
-import { API_URL } from "../../constans";
+import { Loader } from "../../components/Loader";
+import { API_URL } from "../../constants";
 // import cls from "./HomePage.module.css";
 import { QuestionCardList } from "../../components/QuestionCardList";
+//import { delayFn } from "../../helpers/delayFn";
+import { useFetch } from "../../hooks/useFetch";
 
 export const HomePage = () => {
   const [cards, setCards] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  const getQuestions = async () => {
-    try {
-      const res = await fetch(`${API_URL}/react`);
-      const data = await res.json();
-      setCards(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [getQuestions, isLoading, error] = useFetch(async (url) => {
+    const response = await fetch(`${API_URL}/${url}`);
+    const data = await response.json();
+    console.log(data);
+    setCards(data);
+    return data;
+  });
+
+  // const getQuestions = async () => {
+  //   try {
+  //     setIsLoading(true);
+  //     await delayFn();
+  //     const res = await fetch(`${API_URL}/react`);
+  //     const data = await res.json();
+  //     setCards(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
 
   useEffect(() => {
-    getQuestions();
+    getQuestions("react");
   }, []);
 
   return (
     <>
-      {/* <QuestionCard /> */}
-      {/* {cards.map((card) => (
-        <QuestionCard key={card.id} card={card} />
-      ))} */}
-      <QuestionCardList cards={cards} />
+      {isLoading && <Loader />}
+      {error && <p>{error}</p>}
+      {!isLoading && <QuestionCardList cards={cards} />}
     </>
   );
 };
